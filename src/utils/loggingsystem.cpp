5d0b8127f0a339b2ae9b6a4dc8eeb809ae0db469
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2025-06-21 22:48:51 -0600

    add: Added start of LoggingSystem class

---
+++ src/utils/loggingsystem.cpp
@@ -0,0 +1,53 @@
+/*********************************************************************
+* @file   loggingsystem.cpp
+* @brief  Logging system for the plugin.
+* @author Orsell
+* @date   06 2025
+*********************************************************************/
+
+#include "stdafx.hpp"   
+#include "utils/loggingsystem.hpp"
+
+#include "commands.hpp"
+#include ""
+
+/**
+ * @brief Logging for the plugin by adding a prefix and line break.
+ * @param level Log level for log message. 0 = Msg/DevMsg, 1 = Warning/DevWarning, 2 = Error WILL STOP ENGINE!
+ * @param dev Whether this log message should only appear when "p2sm_developer" is enabled.
+ * @param message Message to log. Max character limit of 1024 characters.	
+ * @param ... Message formatting.
+ */
+void Log(const LogLevel level, const bool dev, const char* message, ...)
+{
+    if (dev && !p2sm_developer.GetBool() && level != ERROR)
+        return; // Stop developer messages when p2mm_developer isn't enabled.
+
+    // Take our log message and format any arguments it has into the message.
+    va_list argPtr;
+    char szFormattedText[1024] = { 0 };
+    va_start(argPtr, message);
+    vsnprintf(szFormattedText, sizeof(szFormattedText), message, argPtr);
+    va_end(argPtr);
+
+    // Add a header to the log message.
+    char completeMsg[1024] = { 0 };
+    snprintf(completeMsg, sizeof(completeMsg), "(P2SourceMod++ PLUGIN): %s\n", szFormattedText);
+
+    switch (level)
+    {
+    case (INFO):
+        ConColorMsg(P2SMPLUSPLUS_PLUGIN_CONSOLE_COLOR, completeMsg);
+        return;
+    case (WARNING):
+        Warning(completeMsg);
+        return;
+    case (ERROR):
+        Warning("(P2SourceMod++ PLUGIN):\n!!!ERROR ERROR ERROR!!!:\nA FATAL ERROR OCCURED WITH THE ENGINE:\n%s", completeMsg);
+        Error(completeMsg);
+        return;
+    default:
+        Warning("(P2SourceMod++ PLUGIN): Log level set outside of INFO-ERROR, \"%i\". Defaulting to level INFO.\n", level);
+        ConColorMsg(P2SMPLUSPLUS_PLUGIN_CONSOLE_COLOR, completeMsg);
+    }
+}
---
+++ src/utils/loggingsystem.hpp
@@ -0,0 +1,60 @@
+/*********************************************************************
+* @file   loggingsystem.hpp
+* @brief  Logging system for the plugin.
+* @author Orsell
+* @date   06 2025
+*********************************************************************/
+
+#pragma once
+
+#ifndef LOGGING_HPP
+#define LOGGING_HPP
+
+#define P2SMPLUSPLUS_PLUGIN_CONSOLE_COLOR  Color(100, 192, 252, 255) // Light Blue
+
+#undef ERROR // ERROR is a Windows header defined macro that is interfering with the enum below
+/**
+ * @brief Log levels for all log functions.
+ */
+typedef enum LogLevels : std::uint8_t
+{
+    INFO = 0,
+    WARNING,
+    ERROR
+} LogLevel;
+
+/**
+ * @brief The log channel struct which contains information abotu  
+ */
+typedef struct LogChannel
+{
+    const char* channelName; // Log channel logged messages belong to.
+    Color messageColor;      // Color that the logged messages should print as.
+    bool fileLogged;
+} LogChannel;
+
+class LoggingSystem
+{
+public: // MARK: LoggingSystem Public Members
+#pragma region Public Members
+
+    std::vector<LogChannel> GetLogChannels();
+    bool AddLogChannel(const LogChannel channel);
+    bool RemoveLogChannel(const LogChannel channel);
+    
+#pragma endregion
+
+private: // MARK: LoggingSystem Private Members
+#pragma region Private Members
+
+    std::vector<LogChannel> logChannels;
+    
+#pragma endregion
+};
+
+void Log(LogLevel level, bool dev, const char* message, ...);
+
+// Logging function.
+//void Log(LogLevel level, bool dev, const char* pMsgFormat, ...);
+
+#endif
