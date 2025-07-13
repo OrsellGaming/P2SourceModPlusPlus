489e280c15962dc6df851caa4f451fe2653d2e9b
OrsellGaming <34631691+OrsellGaming@users.noreply.github.com>
2025-07-13 00:46:10 -0600

    feat: Added interface file for defines for viewports

---
+++ src/interfaces/viewport_panel_names.hpp
@@ -0,0 +1,31 @@
+/*******************************************************************
+* @file   viewport_panel_names.hpp
+* @brief  Macro definitions of viewports used by CBasePlayer::ShowViewPortPanel.
+* @author Orsell
+* @date   07 2025
+*********************************************************************/
+
+#pragma once
+
+#ifndef VIEWPORT_HPP
+#define VIEWPORT_HPP
+
+#define PANEL_ALL			            "all"		// all current panels
+#define PANEL_ACTIVE		            "active"	// current active panel
+#define PANEL_SCOREBOARD	            "scores"
+#define PANEL_PLAYERDETAILS	            "playerdetails"
+#define PANEL_OVERVIEW		            "overview"
+#define PANEL_CLASS			            "class"
+#define PANEL_TEAM			            "team"
+#define PANEL_SPECGUI		            "specgui"	// passive spectator elements (top/bottom bars)
+#define PANEL_SPECMENU		            "specmenu"  // active spectator elements (options menus etc)
+#define PANEL_INFO			            "info"
+#define PANEL_NAV_PROGRESS	            "nav_progress"
+#define PANEL_BUYPRESET_MAIN	        "buypreset_main"
+#define PANEL_BUYPRESET_EDIT	        "buypreset_edit"
+#define PANEL_INTRO			            "intro"
+#define PANEL_COMMENTARY_MODELVIEWER	"commentary_modelviewer"
+#define PANEL_SURVEY					"surveypanel"
+#define PANEL_RADIAL_MENU               "radialmenupanel"
+
+#endif
